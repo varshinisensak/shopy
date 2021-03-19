897989d7013baeb1ec68ffd9c026bcec47049c96
varshini-surya <suryavarshini9798@gmail.com>
2021-03-19 18:52:32 +0530

    sv

---
+++ src/component.js/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+    return(
+        <header className="row block center">
+            <div> <h1>shopping platform</h1></div>
+          
+        </header>
+    )
+}
+export default Header; 
